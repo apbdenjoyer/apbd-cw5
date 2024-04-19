cad4d3e614d135d399fe814ee1b123afd5bdd677
Franciszek Gozdecki <s27379@pjwstk.edu.pl>
2024-04-19 22:53:34 +0200

    Added models: * Animal * Visit
    Added controllers:
    * AnimalsController
    * VisitsController

---
+++ apbd-cw5/apbd-cw5/Controllers/VisitsController.cs
@@ -0,0 +1,39 @@
+﻿using apbd_cw5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apbd_cw5.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class VisitsController : ControllerBase
+{
+    private static readonly List<Visit> Visits = new() { };
+
+    [HttpGet("{id:int}")]
+    public IActionResult GetVisits(int id)
+    {
+        var visitsForThisAnimal = new List<Visit>();
+        for (var index = 0; index < Visits.Count; index++)
+        {
+            var visit = Visits[index];
+            if (visit.AnimalID == id)
+            {
+                visitsForThisAnimal.Add(visit);
+            }
+        }
+
+        if (visitsForThisAnimal.Count == 0)
+        {
+            return NotFound($"No visits for animal with id {id}.");
+        }
+
+        return Ok(visitsForThisAnimal);
+    }
+
+    [HttpPost]
+    public IActionResult AddVisit(Visit visit)
+    {
+        Visits.Add(visit);
+        return Created();
+    }
+}
---
+++ apbd-cw5/apbd-cw5/Models/Visit.cs
@@ -0,0 +1,10 @@
+﻿namespace apbd_cw5.Models;
+
+public class Visit
+{
+    public int ID { get; set; }
+    public DateTime Date { get; set; }
+    public int AnimalID { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+}
