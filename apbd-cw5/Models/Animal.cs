ea0c6c79e40d08e9fc01c13368cea35d8f6fe940
Franciszek Gozdecki <s27379@pjwstk.edu.pl>
2024-04-19 23:07:20 +0200

    * Restructured Files

---
+++ apbd-cw5/Models/Animal.cs
@@ -0,0 +1,12 @@
+﻿namespace apbd_cw5.Models;
+
+public class Animal
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public double Mass { get; set; }
+    public string FurColor{ get; set; }
+    
+    
+}
